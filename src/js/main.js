586ae1d865086ce84926a9d65f251329c65f031d
Xuyen <xuyenhuynguyen@gmail.com>
2020-09-22 10:22:25 +0700

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -7,19 +7,52 @@ window.onscroll = function() {
 function scrollFunction() {
 	const headerScrolledTextEls = document.querySelectorAll('#header-scrollText');
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-		document.getElementById('header-scrolled').style.backgroundColor = '#fff';
-		document.getElementById('header-scrolled').style.boxShadow = '0 0.5rem 1rem rgba(0, 0, 0, 0.15)';
+		document.getElementById('mainHead').style.backgroundColor = '#fff';
+		document.getElementById('mainHead').style.boxShadow = '0 0.5rem 1rem rgba(0, 0, 0, 0.15)';
+		document.getElementById('logo-text').style.color = '#f4623a';
 		headerScrolledTextEls.forEach((El) => {
 			El.style.color = '#000';
 		});
 	} else {
-		document.getElementById('header-scrolled').style.backgroundColor = 'transparent';
-		document.getElementById('header-scrolled').style.boxShadow = 'none';
+		document.getElementById('mainHead').style.backgroundColor = 'transparent';
+		document.getElementById('mainHead').style.boxShadow = 'none';
+		document.getElementById('logo-text').style.color = 'rgba(255, 255, 255, 0.7)';
 		headerScrolledTextEls.forEach((El) => {
 			El.style.color = 'rgba(255, 255, 255, 0.7)';
 		});
 	}
 }
+
+//vanila menu
+$(document).ready(function() {
+	$('a[href*=#]').bind('click', function(e) {
+		e.preventDefault(); // prevent hard jump, the default behavior
+
+		var target = $(this).attr('href'); // Set the target as variable
+
+		// perform animated scrolling by getting top-position of target-element and set it as scroll target
+		$('html, body').stop().animate({
+			scrollTop: $(target).offset().top
+		}, 600, function() {
+			location.hash = target; //attach the hash (#jumptarget) to the pageurl
+		});
+
+		return false;
+	});
+});
+
+$(window)
+	.scroll(function() {
+		var scrollDistance = $(window).scrollTop();
+		// Assign active class to nav links while scolling
+		$('.section').each(function(i) {
+			if ($(this).position().top <= scrollDistance + 72) {
+				$('#mainHead .navbar__link.active').removeClass('active');
+				$('#mainHead .navbar__link').eq(i).addClass('active');
+			}
+		});
+	})
+	.scroll();
 
 //burger click show menu
 function navMenu() {
